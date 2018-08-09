db9d07365441d6bfa2a1a201a343463d6f888466
Richard Westenra <rjwestenra@gmail.com>
2018-08-10 10:29:14 +1200

    Add readme and improve JS readability

---
+++ README.md
@@ -0,0 +1,13 @@
+# Speed Test
+
+Run a speed test every ten minutes using Node.js and the [speedtest-net package](https://github.com/ddsol/speedtest.net), and log the results to a CSV.
+
+## Usage
+Clone the repository, install dependencies, and start it up:
+```
+git clone git@github.com:richardwestenra/speed-test.git
+cd speed-test
+npm install
+npm start
+```
+Results are logged to data.csv. You can change the interval and the log file in index.js.
--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 const fs = require('fs');
 const speedTest = require('speedtest-net');
+process.env['HTTP_PROXY'] =  process.env['http_proxy'] = 'http://proxy:3128';
 
-process.env["HTTP_PROXY"] =  process.env["http_proxy"] = "http://proxy:3128";
+const interval = 10; // minutes
+const destination = 'data.csv'; // data log file
 
 const time = () => new Date().toISOString();
+
+const minsToMs = m => m * 60 * 1000;
 
 const appendData = (data, index) => {
   const now = time();
   const newDatum = `\n${now}, ${data.speeds.download}, ${data.speeds.upload}`;
 
-  fs.appendFile('data.csv', newDatum, function (error) {
+  fs.appendFile(destination, newDatum, function (error) {
     if (error) throw error;
-    console.log(`Saved to file ${index} (${now})`);
+    console.log(`Saved to file ${index} - ${now}`);
   });
 };
 
+// Run a new test
 const run = index => {
-  console.log(`Run new test ${index} (${time()})`);
+  console.log(`Run new test ${index} - ${time()}`);
   const test = speedTest({ maxTime: 5000 });
 
   test.on('data', data => {
     appendData(data, index);
-    setTimeout(run.bind(this, index + 1), 10 * 60 * 1000);
+    setTimeout(run.bind(this, index + 1), minsToMs(interval));
   });
 
-  test.on('error', err => {
-    console.error(err);
-  });
+  test.on('error', console.error);
 };
 
 run(1);
 
 // Keep alive for 24 hours
-setTimeout(console.log, 24 * 60 * 60 * 1000);
+setTimeout(console.log, minsToMs(24 * 60));
